3428da837b350b06f8f57da92a58b3cff5a1f3fe
Brano Zarnovican <zarnovican@gmail.com>
2016-01-08 14:00:11 +0100

    dump OS env on /

--- helloapp/views.py
+++ helloapp/views.py
@@ -1,8 +1,14 @@
+import os
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h2>Hello, world!</h2>")
+    out=''
+    for k, v in os.environ.items():
+        out += '{}={}\n'.format(k, v)
+
+    return HttpResponse("<h2>Hello, world!</h2> <pre>"+out+"</pre>")
 
