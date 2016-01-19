a3e30e2a87afce36893a5c40a74bb691b2ccec12
Brano Zarnovican <zarnovican@gmail.com>
2016-01-19 10:55:51 +0100

    view for 'env' and slow get

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^env$', views.env),
+    url(r'^slow/(?P<time_to_wait>[0-9]+)$', views.slow_get),
 ]
--- helloapp/views.py
+++ helloapp/views.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from django.shortcuts import render
 
@@ -6,9 +7,21 @@ from django.http import HttpResponse
 
 
 def index(request):
+    return HttpResponse("<h2>Hello, world!</h2>")
+
+
+def env(request):
     out=''
     for k, v in sorted(os.environ.items()):
         out += '{}={}\n'.format(k, v)
 
-    return HttpResponse("<h2>Hello, world!</h2> <pre>"+out+"</pre>")
+    return HttpResponse("<h2>Environment:</h2> <pre>"+out+"</pre>")
+
+
+def slow_get(request, time_to_wait):
+    out = ''
+    for i in range(int(time_to_wait)):
+        out += '.'
+        time.sleep(1)
+    return HttpResponse("{}. Responded after {} seconds.".format(out, time_to_wait))
 
