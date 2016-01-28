2eb2b07d0d429e7d9d9e01004d666af4a48cd53d
Brano Zarnovican <zarnovican@gmail.com>
2016-01-28 13:44:16 +0100

    random startup delay

--- helloapp/__init__.py
+++ helloapp/__init__.py
@@ -1,3 +1,10 @@
 
+import random
+import time
+
 print('Importing helloapp/__init__.py')
 
+delay = random.randint(2, 5)
+for i in range(delay):
+    print('Slowly starting app.. {}/{}'.format(i+1, delay))
+    time.sleep(1)
