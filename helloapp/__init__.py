4bf412aabe0a8534b84f61d6fa23560b05d8f24d
Brano Zarnovican <zarnovican@gmail.com>
2016-01-28 19:25:43 +0100

    print pid during startup

--- helloapp/__init__.py
+++ helloapp/__init__.py
@@ -1,9 +1,10 @@
 
+import os
 import time
 
 print('Importing helloapp/__init__.py')
 
 delay = 10
 for i in range(delay):
-    print('Slowly starting app.. {}/{}'.format(i+1, delay))
+    print('pid {}: Slowly starting app.. {}/{}'.format(os.getpid(), i+1, delay))
     time.sleep(1)
