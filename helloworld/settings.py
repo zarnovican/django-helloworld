d70d49106875ecea5f56cb83cd43030f654f253f
Brano Zarnovican <zarnovican@gmail.com>
2016-02-12 12:03:41 +0100

    static file with version string

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -101,5 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 HELLOWORLD_VERSION = '@@VERSION@@'
