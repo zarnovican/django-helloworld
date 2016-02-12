2893abe8b49bd8590925ff689113c32dda890b45
Brano Zarnovican <zarnovican@gmail.com>
2016-02-12 12:58:15 +0100

    simulate slowly starting app

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from django.apps import AppConfig
 
 print('importing helloworld/app.py')
@@ -7,3 +10,7 @@ class HelloworldConfig(AppConfig):
 
     def ready(self):
         print('Running ready() on helloworld app')
+        delay = 5
+        for i in range(delay):
+            print('pid {}: Slowly starting app.. {}/{}'.format(os.getpid(), i+1, delay))
+            time.sleep(1)
