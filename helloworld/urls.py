629c1b298bf7372d74b4a6d814ae1b5c5f59e299
Brano Zarnovican <zarnovican@gmail.com>
2016-02-12 11:00:44 +0100

    view imitating a slow GET

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^env$', views.env, name='env'),
+    url(r'^slow/(?P<time_to_wait>[0-9]+)$', views.slow_get, name='slow_get'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,11 +1,22 @@
 
 import os
+import time
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'helloworld/index.html')
 
+
 def env(request):
     return render(request, 'helloworld/env.html', {'environ': sorted(os.environ.items())})
+
+
+def slow_get(request, time_to_wait):
+    out = ''
+    for i in range(int(time_to_wait)):
+        out += '.'
+        time.sleep(1)
+    return HttpResponse("{}. Responded after {} seconds.".format(out, time_to_wait))
