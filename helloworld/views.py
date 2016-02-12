3d4583b4d87356e731f55694e51d2f51b0c3e31d
Brano Zarnovican <zarnovican@gmail.com>
2016-02-12 11:09:29 +0100

    view to return app version from code and settings

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,9 +2,11 @@
 import os
 import time
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
+VERSION = '@@VERSION@@'
 
 def index(request):
     return render(request, 'helloworld/index.html')
@@ -20,3 +22,7 @@ def slow_get(request, time_to_wait):
         out += '.'
         time.sleep(1)
     return HttpResponse("{}. Responded after {} seconds.".format(out, time_to_wait))
+
+
+def version(request):
+    return HttpResponse("settings: {} - code: {}".format(settings.HELLOWORLD_VERSION, VERSION))
