f35fba311eb142a439861182c7524083972510ea
Brano Zarnovican <zarnovican@gmail.com>
2016-02-12 10:28:38 +0100

    env view to dump environment vars

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^env$', views.env, name='env'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,6 +1,11 @@
 
+import os
+
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'helloworld/index.html')
+
+def env(request):
+    return render(request, 'helloworld/env.html', {'environ': sorted(os.environ.items())})
