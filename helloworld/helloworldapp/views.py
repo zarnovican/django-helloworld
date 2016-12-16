f7fab04ccd05c2925abee22fb648ead40d0e4db6
Brano Zarnovican <zarnovican@gmail.com>
2016-12-16 19:28:32 +0100

    10% chance of 403 on GET /

--- helloworld/helloworldapp/views.py
+++ helloworld/helloworldapp/views.py
@@ -1,5 +1,6 @@
 import logging
-from django.http import HttpResponse
+import random
+from django.http import HttpResponse, HttpResponseForbidden
 
 from . import metric
 
@@ -8,4 +9,6 @@ logger = logging.getLogger(__name__)
 @metric.request_latency_seconds.time()
 def index(request):
     logger.debug('index viewed')
+    if random.randint(0, 100) < 10:
+        return HttpResponseForbidden()
     return HttpResponse("Hello, world. You're at the helloworld index.")
