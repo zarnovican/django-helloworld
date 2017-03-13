9c3e2961c7af067fa9ad187341314783d891d159
Brano Zarnovican <zarnovican@gmail.com>
2017-03-13 08:59:29 +0100

    added /info endpoint

--- helloworldapp/views.py
+++ helloworldapp/views.py
@@ -1,5 +1,8 @@
 import logging
 import random
+import socket
+
+from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden
 
 from metric import REQUEST_TIME, REQUEST_COUNT
@@ -12,3 +15,15 @@ def index(request):
 
 # TODO: I wasn't able to write this as decorator
 index = REQUEST_TIME.labels(url='index').time()(index)
+
+def get_info(request):
+    REQUEST_COUNT.inc()
+
+    iam = settings.SERVICE_NAME
+    if settings.DOCKER_TASK_SLOT:
+        iam += '.{}'.format(settings.DOCKER_TASK_SLOT)
+
+    return HttpResponse('{} (<version>) on {}: your IP {}\n'.format(
+        iam, socket.gethostname(), request.META['REMOTE_ADDR']), content_type='text/plain')
+
+get_info = REQUEST_TIME.labels(url='index').time()(get_info)
