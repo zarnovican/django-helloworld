fcd22c60330144df59958b81bde4993cd5b5f577
Brano Zarnovican <zarnovican@gmail.com>
2017-03-17 14:24:30 +0100

    slowly starting app, added 10s delay

--- helloworldapp/apps.py
+++ helloworldapp/apps.py
@@ -1,5 +1,7 @@
 
 import logging
+import time
+
 from setuptools_scm import get_version
 
 from django.apps import AppConfig
@@ -14,6 +16,9 @@ class HelloworldappConfig(AppConfig):
     name = 'helloworldapp'
 
     def ready(self):
+        for i in range(10):
+            logger.info('Slowly starting app %d/10', i+1)
+            time.sleep(1)
         logger.info('Application ready')
         settings.VERSION = get_version()
         setup_metrics(settings)
